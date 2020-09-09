c519a06f7cf63f8aad8707f3600fbdf557dd189d
Admin-is-traitor <afzaal.e01@gmail.com>
2020-09-09 15:24:38 +0500

    Views Work Completed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 
@@ -38,8 +38,22 @@ class HelloApiView(APIView):
         """Handle Updating an Object"""
         return Response({'method':'PUT'})
     def patch(self,request,pk=None):
-        """Hanlde Updating Object partially"""
+        """Handle Updating Object partially"""
         return Response({'method':'patch'})
     def delete(self,request,pk=None):
         """Handle Delete an Object"""
         return Response({'method':'Delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API Viewset"""
+    def list(self,request):
+        """Return Some String for massage passing test"""
+
+        a_viewset = [
+                        'Uses actions (list, create, retrieve, update, partial_update)',
+                        'Automatically maps to URLs using Routers',
+                        'Provides more functionality with less code',
+                    ]
+
+        return Response({'massage':'Hello!','a_viewset':a_viewset})
